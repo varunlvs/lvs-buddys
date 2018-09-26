cf94304210d66ca5b3ca9750de065df1f8de2a4b
varunlvs <42297561+varunlvs@users.noreply.github.com>
2018-09-26 21:56:36 +0530

    Update

--- src/robots.js
+++ src/robots.js
@@ -1,123 +1,566 @@
 export const robots = [
   {
     id: 1,
-    name: 'Mohamed Suhal',
-    username: 'Suhal',
-    email: 'Suhal@april.biz'
+    name: 'Naveen Kumar',
+    username: 'naveen',
+    email: 'naveen@april.biz'
   },
   {
     id: 2,
-    name: 'Kathirvel',
-    username: 'Kathir',
-    email: 'kathir@melissa.tv'
+    name: 'Abishek',
+    username: 'Abishek',
+    email: 'abishek@melissa.tv'
   },
   {
     id: 3,
-    name: 'Kaviarasu Vasu',
-    username: 'Kaviarasu',
-    email: 'kavi@yesenia.net'
+    name: 'Aishwariya S',
+    username: 'Aishwariya',
+    email: 'aishwariya@yesenia.net'
   },
   {
     id: 4,
-    name: 'Varapradhan',
+    name: 'Anbu Suriya',
+    username: 'Suriya',
+    email: 'suriya@kory.org'
+  },
+  {
+    id: 5,
+    name: 'Arun Kumar',
+    username: 'Arun',
+    email: 'arun@annie.ca'
+  },
+  {
+    id: 6,
+    name: 'Calpana',
+    username: 'Calpana',
+    email: 'calpana@jasper.info'
+  },
+  {
+    id: 7,
+    name: 'Charan Mithesh',
+    username: 'Charan.Skiles',
+    email: 'charan@billy.biz'
+  },
+  {
+    id: 8,
+    name: 'Charumathi',
+    username: 'Charumathi',
+    email: 'charumathi@rosamond.me'
+  },
+  {
+    id: 9,
+    name: 'Deepika V',
+    username: 'Deepika',
+    email: 'deepika@dana.io'
+  },
+  {
+    id: 10,
+    name: 'Dharani',
+    username: 'dharani',
+    email: 'dharani@karina.biz'
+  },
+  {
+    id: 11,
+    name: 'Aadith',
+    username: 'Aadith',
+    email: 'aadith@karina.biz'
+  },
+  {
+    id: 12,
+    name: 'Aiswariya M',
+    username: 'Aiswariya',
+    email: 'aiswariya@billy.biz'
+  },
+  {
+    id: 13,
+    name: 'Arvind',
+    username: 'Aravind',
+    email: 'aravind@dana.io'
+  },
+  {
+    id: 14,
+    name: 'Ashwini',
+    username: 'Ashwini',
+    email: 'Ashwini@jasper.info'
+  },
+  {
+    id: 15,
+    name: 'Deepika',
+    username: 'Deepika',
+    email: 'Deepika@dana.me'
+  },
+  {
+    id: 16,
+    name: 'Dhanuja',
+    username: 'Dhanuja',
+    email: 'Dhanuja@kory.me'
+  },
+  {
+    id: 17,
+    name: 'Dhanumaalian',
+    username: 'Dhanumaalian',
+    email: 'Dhanumaalian@billy.me'
+  },
+  {
+    id: 18,
+    name: 'Dharani Kumar',
+    username: 'Dharani',
+    email: 'Dharani@rosamond.me'
+  },
+  {
+    id: 19,
+    name: 'Divya',
+    username: 'Divya',
+    email: 'Divya@karina.me'
+  },
+  {
+    id: 20,
+    name: 'Gayathri',
+    username: 'Gayathri',
+    email: 'Gayathri@april.me'
+  },
+  {
+    id: 21,
+    name: 'Dharshini',
+    username: 'Dharshini',
+    email: 'Dharshini@april.biz'
+  },
+  {
+    id: 22,
+    name: 'Dinesh Kumar',
+    username: 'Dinesh',
+    email: 'Dinesh@melissa.tv'
+  },
+  {
+    id: 23,
+    name: 'Diwakar',
+    username: 'Diwakar',
+    email: 'Diwakar@yesenia.net'
+  },
+  {
+    id: 24,
+    name: 'Durga',
+    username: 'Durga',
+    email: 'Durga@kory.org'
+  },
+  {
+    id: 25,
+    name: 'Gowdham',
+    username: 'Gowdham',
+    email: 'Gowdham@annie.ca'
+  },
+  {
+    id: 26,
+    name: 'Hariharan',
+    username: 'Hariharan',
+    email: 'Hariharan@jasper.info'
+  },
+  {
+    id: 27,
+    name: 'Harish BT',
+    username: 'Harish.Skiles',
+    email: 'Harish@billy.biz'
+  },
+  {
+    id: 28,
+    name: 'Harish Kumar',
+    username: 'Harish',
+    email: 'Harish@rosamond.me'
+  },
+  {
+    id: 29,
+    name: 'Ilamathi',
+    username: 'Ilamathi',
+    email: 'Ilamathi@dana.io'
+  },
+  {
+    id: 30,
+    name: 'Jayapradha',
+    username: 'Jayapradha',
+    email: 'Jayapradha@karina.biz'
+  },
+  {
+    id: 31,
+    name: 'Girivarman',
+    username: 'Girivarman',
+    email: 'Girivarman@karina.biz'
+  },
+  {
+    id: 32,
+    name: 'Gokulnath',
+    username: 'Gokulnath',
+    email: 'Gokulnath@billy.biz'
+  },
+  {
+    id: 33,
+    name: 'Karthick',
+    username: 'Karthick',
+    email: 'Karthick@dana.io'
+  },
+  {
+    id: 34,
+    name: 'Karthigeyan',
+    username: 'Karthigeyan',
+    email: 'Karthigeyan@jasper.info'
+  },
+  {
+    id: 35,
+    name: 'Krithika',
+    username: 'krithika',
+    email: 'krithika@dana.me'
+  },
+  {
+    id: 36,
+    name: 'Nagaraj',
+    username: 'Nagaraj',
+    email: 'Nagaraj@kory.me'
+  },
+  {
+    id: 37,
+    name: 'Nasreen',
+    username: 'Nasreen',
+    email: 'Nasreen@billy.me'
+  },
+  {
+    id: 38,
+    name: 'Nithyanandham',
+    username: 'Nithyanandham',
+    email: 'Nithyanandham@rosamond.me'
+  },
+  {
+    id: 39,
+    name: 'Mahalakshmi',
+    username: 'Mahalakshmi',
+    email: 'Mahalakshmi@karina.me'
+  },
+  {
+    id: 40,
+    name: 'Pierre Jean Elvis',
+    username: 'Elvis',
+    email: 'Elvis@april.me'
+  },
+  {
+    id: 41,
+    name: 'Jayashree',
+    username: 'Jayashree',
+    email: 'Jayashree@april.biz'
+  },
+  {
+    id: 42,
+    name: 'Karthik',
+    username: 'Karthik',
+    email: 'Karthik@melissa.tv'
+  },
+  {
+    id: 43,
+    name: 'Karthikeyan',
+    username: 'Karthikeyan',
+    email: 'Karthikeyan@yesenia.net'
+  },
+  {
+    id: 44,
+    name: 'Kathirvel',
+    username: 'Kathirvel',
+    email: 'Kathirvel@kory.org'
+  },
+  {
+    id: 45,
+    name: 'Kaviarasu',
+    username: 'Kaviarasu',
+    email: 'Kaviarasu@annie.ca'
+  },
+  {
+    id: 46,
+    name: 'Krithika',
+    username: 'Krithika',
+    email: 'Krithika@jasper.info'
+  },
+  {
+    id: 47,
+    name: 'Lakshmi Narasimha Rao Karri',
+    username: 'Rao.Skiles',
+    email: 'Rao@billy.biz'
+  },
+  {
+    id: 48,
+    name: 'Lara Iyswarya Rane',
+    username: 'lara',
+    email: 'lara@rosamond.me'
+  },
+  {
+    id: 49,
+    name: 'Madhanraj',
+    username: 'Madhanraj',
+    email: 'Madhanraj@dana.io'
+  },
+  {
+    id: 50,
+    name: 'Muthu Kumaran',
+    username: 'Muthu',
+    email: 'Muthu@karina.biz'
+  },
+  {
+    id: 51,
+    name: 'Pooja',
+    username: 'pooja',
+    email: 'pooja@karina.biz'
+  },
+  {
+    id: 52,
+    name: 'Prem kumar',
+    username: 'prem',
+    email: 'prem@billy.biz'
+  },
+  {
+    id: 53,
+    name: 'Priya',
+    username: 'priya',
+    email: 'priya@dana.io'
+  },
+  {
+    id: 54,
+    name: 'Raghavi Muthu',
+    username: 'Raghavi',
+    email: 'Raghavi@jasper.info'
+  },
+  {
+    id: 55,
+    name: 'Ram Kumar',
+    username: 'ram',
+    email: 'ram@dana.me'
+  },
+  {
+    id: 56,
+    name: 'Sainath',
+    username: 'sainath',
+    email: 'sainath@kory.me'
+  },
+  {
+    id: 57,
+    name: 'Sathiyabama',
+    username: 'Sathiyabama',
+    email: 'Sathiyabama@billy.me'
+  },
+  {
+    id: 58,
+    name: 'Shanmuga Priya',
+    username: 'Priya',
+    email: 'priya@rosamond.me'
+  },
+  {
+    id: 59,
+    name: 'Sathya Narayanan',
+    username: 'Sathya',
+    email: 'Sathya@karina.me'
+  },
+  {
+    id: 60,
+    name: 'Shivani Swaroop',
+    username: 'shivani',
+    email: 'shivani@april.me'
+  },
+  {
+    id: 61,
+    name: 'Naveen',
+    username: 'Naveen',
+    email: 'Naveen@april.biz'
+  },
+  {
+    id: 62,
+    name: 'Nivedha L',
+    username: 'Nivedha',
+    email: 'Nivedha@melissa.tv'
+  },
+  {
+    id: 63,
+    name: 'Nivedha R',
+    username: 'Nivedha',
+    email: 'Nivedha@yesenia.net'
+  },
+  {
+    id: 64,
+    name: 'Shanmuga Vijay',
+    username: 'Shan',
+    email: 'shan@kory.org'
+  },
+  {
+    id: 65,
+    name: 'Shanmugha Priya',
+    username: 'SP',
+    email: 'sp@annie.ca'
+  },
+  {
+    id: 66,
+    name: 'Sindhu Ruby',
+    username: 'Ruby',
+    email: 'Ruby@jasper.info'
+  },
+  {
+    id: 67,
+    name: 'Sivasankari',
+    username: 'Sivasankari.Skiles',
+    email: 'Sivasankari@billy.biz'
+  },
+  {
+    id: 68,
+    name: 'Sowrrow Singh',
+    username: 'Sowrrow',
+    email: 'Sowrrow@rosamond.me'
+  },
+  {
+    id: 69,
+    name: 'Sri Varapradhan',
     username: 'Varapradhan',
     email: 'varapradhan@kory.org'
   },
   {
-    id: 5,
-    name: 'Naveen Murugan',
-    username: 'Naveen',
-    email: 'naveen@annie.ca'
-  },
-  {
-    id: 6,
-    name: 'Vegatesa Perumal',
-    username: 'Venky',
-    email: 'venky@jasper.info'
-  },
-  {
-    id: 7,
-    name: 'Madhan',
-    username: 'madhan.Skiles',
-    email: 'madhan@billy.biz'
-  },
-  {
-    id: 8,
-    name: 'Vignesh',
-    username: 'Vicky',
-    email: 'vignesh@rosamond.me'
-  },
-  {
-    id: 9,
-    name: 'Sowrab Singh',
-    username: 'Sowrab',
-    email: 'Sowrab@dana.io'
-  },
-  {
-    id: 10,
-    name: 'Varun Prakash',
-    username: 'varun',
-    email: 'varun@karina.biz'
-  },
-  {
-    id: 11,
-    name: 'Arun Kumar',
-    username: 'arun',
-    email: 'arun@karina.biz'
-  },
-  {
-    id: 12,
-    name: 'Diwakar',
-    username: 'diwakar',
-    email: 'diwakar@billy.biz'
-  },
-  {
-    id: 13,
-    name: 'Gowtham',
-    username: 'gowtham',
-    email: 'gowtham@dana.io'
-  },
-  {
-    id: 14,
-    name: 'Harish',
-    username: 'harish',
-    email: 'harish@jasper.info'
-  },
-  {
-    id: 15,
-    name: 'Charan',
-    username: 'charan',
-    email: 'charan@rosamond.me'
-  },
-  {
-    id: 16,
-    name: 'Dinesh Kumar',
-    username: 'dinesh',
-    email: 'dinesh@rosamond.me'
-  },
-  {
-    id: 17,
+    id: 70,
     name: 'Suresh Guru',
     username: 'suresh',
-    email: 'suresh@rosamond.me'
+    email: 'suresh@karina.biz'
   },
   {
-    id: 18,
-    name: 'Karthick R',
-    username: 'karthick',
-    email: 'karthick@rosamond.me'
+    id: 71,
+    name: 'Sri Harini',
+    username: 'Harini',
+    email: 'Harini@karina.biz'
   },
   {
-    id: 19,
-    name: 'Sudharsan R',
-    username: 'sudha',
-    email: 'sudha@rosamond.me'
+    id: 72,
+    name: 'Sriram',
+    username: 'Sriram',
+    email: 'Sriram@billy.biz'
   },
   {
-    id: 20,
-    name: 'Nagaraj R',
-    username: 'nagaraj',
-    email: 'nagaraj@rosamond.me'
+    id: 73,
+    name: 'Stephin Radj',
+    username: 'Stephin',
+    email: 'Stephin@dana.io'
+  },
+  {
+    id: 74,
+    name: 'Sudharsan',
+    username: 'Sudharsan',
+    email: 'Sudharsan@jasper.info'
+  },
+  {
+    id: 75,
+    name: 'Suria Priya',
+    username: 'Priya',
+    email: 'Priya@dana.me'
+  },
+  {
+    id: 76,
+    name: 'Tharrshinne',
+    username: 'Tharrshinne',
+    email: 'Tharrshinne@kory.me'
+  },
+  {
+    id: 77,
+    name: 'Thirumalai Vasan',
+    username: 'Thirumalai',
+    email: 'Thirumalai@billy.me'
+  },
+  {
+    id: 78,
+    name: 'Vengata Balaji',
+    username: 'Balaji',
+    email: 'Balaji@rosamond.me'
+  },
+  {
+    id: 79,
+    name: 'Yogesh Kumar',
+    username: 'Yogesh',
+    email: 'Yogesh@karina.me'
+  },
+  {
+    id: 80,
+    name: 'Yogeshwar',
+    username: 'Yogeshwar',
+    email: 'Yogeshwar@april.me'
+  },
+  {
+    id: 81,
+    name: 'Uma',
+    username: 'Uma',
+    email: 'Uma@april.biz'
+  },
+  {
+    id: 82,
+    name: 'Varun Prakash',
+    username: 'Varun',
+    email: 'Varun@melissa.tv'
+  },
+  {
+    id: 83,
+    name: 'Vengatesa Perumal',
+    username: 'Venky',
+    email: 'venky@yesenia.net'
+  },
+  {
+    id: 84,
+    name: 'Vigneshwaran',
+    username: 'Vigneshwaran',
+    email: 'Vigneshwaran@kory.org'
+  },
+  {
+    id: 85,
+    name: 'Vishalipriya',
+    username: 'Vishalipriya',
+    email: 'Vishalipriya@annie.ca'
+  },
+  {
+    id: 86,
+    name: 'Mohamed Suhal',
+    username: 'suhal',
+    email: 'suhal@jasper.info'
+  },
+  {
+    id: 87,
+    name: 'Yves Francis',
+    username: 'yves.Skiles',
+    email: 'yves@billy.biz'
+  },
+  {
+    id: 88,
+    name: 'Yuvaraj',
+    username: 'yuvaraj',
+    email: 'yuvaraj@rosamond.me'
+  },
+  {
+    id: 89,
+    name: 'Yuvasree',
+    username: 'yuvasree',
+    email: 'yuvasree@dana.io'
+  },
+  {
+    id: 90,
+    name: 'Ganesh Raghav',
+    username: 'ganesh',
+    email: 'ganesh@karina.biz'
+  },
+  {
+    id: 91,
+    name: 'Uma Maheswari',
+    username: 'uma',
+    email: 'uma@karina.biz'
+  },
+  {
+    id: 92,
+    name: 'Parameshwaran',
+    username: 'Parameshwaran',
+    email: 'Parameshwaran@billy.biz'
+  },
+  {
+    id: 93,
+    name: 'Vinitha',
+    username: 'vinitha',
+    email: 'vinitha@dana.io'
+  },
+  {
+    id: 94,
+    name: 'Yamuna',
+    username: 'yamuna',
+    email: 'yamuna@jasper.info'
   }
-
 ];
