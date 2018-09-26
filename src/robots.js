f91fdfb4c018e2a7aa229cc3b3e1d1055b49b7bd
varunlvs <42297561+varunlvs@users.noreply.github.com>
2018-09-26 06:09:04 +0530

    initial commit

---
+++ src/robots.js
@@ -0,0 +1,123 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Mohamed Suhal',
+    username: 'Suhal',
+    email: 'Suhal@april.biz'
+  },
+  {
+    id: 2,
+    name: 'Kathirvel',
+    username: 'Kathir',
+    email: 'kathir@melissa.tv'
+  },
+  {
+    id: 3,
+    name: 'Kaviarasu Vasu',
+    username: 'Kaviarasu',
+    email: 'kavi@yesenia.net'
+  },
+  {
+    id: 4,
+    name: 'Varapradhan',
+    username: 'Varapradhan',
+    email: 'varapradhan@kory.org'
+  },
+  {
+    id: 5,
+    name: 'Naveen Murugan',
+    username: 'Naveen',
+    email: 'naveen@annie.ca'
+  },
+  {
+    id: 6,
+    name: 'Vegatesa Perumal',
+    username: 'Venky',
+    email: 'venky@jasper.info'
+  },
+  {
+    id: 7,
+    name: 'Madhan',
+    username: 'madhan.Skiles',
+    email: 'madhan@billy.biz'
+  },
+  {
+    id: 8,
+    name: 'Vignesh',
+    username: 'Vicky',
+    email: 'vignesh@rosamond.me'
+  },
+  {
+    id: 9,
+    name: 'Sowrab Singh',
+    username: 'Sowrab',
+    email: 'Sowrab@dana.io'
+  },
+  {
+    id: 10,
+    name: 'Varun Prakash',
+    username: 'varun',
+    email: 'varun@karina.biz'
+  },
+  {
+    id: 11,
+    name: 'Arun Kumar',
+    username: 'arun',
+    email: 'arun@karina.biz'
+  },
+  {
+    id: 12,
+    name: 'Diwakar',
+    username: 'diwakar',
+    email: 'diwakar@billy.biz'
+  },
+  {
+    id: 13,
+    name: 'Gowtham',
+    username: 'gowtham',
+    email: 'gowtham@dana.io'
+  },
+  {
+    id: 14,
+    name: 'Harish',
+    username: 'harish',
+    email: 'harish@jasper.info'
+  },
+  {
+    id: 15,
+    name: 'Charan',
+    username: 'charan',
+    email: 'charan@rosamond.me'
+  },
+  {
+    id: 16,
+    name: 'Dinesh Kumar',
+    username: 'dinesh',
+    email: 'dinesh@rosamond.me'
+  },
+  {
+    id: 17,
+    name: 'Suresh Guru',
+    username: 'suresh',
+    email: 'suresh@rosamond.me'
+  },
+  {
+    id: 18,
+    name: 'Karthick R',
+    username: 'karthick',
+    email: 'karthick@rosamond.me'
+  },
+  {
+    id: 19,
+    name: 'Sudharsan R',
+    username: 'sudha',
+    email: 'sudha@rosamond.me'
+  },
+  {
+    id: 20,
+    name: 'Nagaraj R',
+    username: 'nagaraj',
+    email: 'nagaraj@rosamond.me'
+  }
+
+];
